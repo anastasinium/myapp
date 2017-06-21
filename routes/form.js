20a07b6e6705f344d8667d72e482d6fae8a236b0
Anastasia <nastia21.11@gmail.com>
2017-06-21 23:59:08 +0300

    ..

--- routes/form.js
+++ routes/form.js
@@ -1,18 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+
 // function findFactorial(n) {
-//     return (n != 1) ? n * factorial(n - 1) : 1;
+//     return n ? n * findFactorial(n - 1) : 1;
 // }
 
 router.post('/', function (req, res) {
 
-  var factorial = findFactorial(req.query.login);
-  // factorial = 23;
+    // req.query.factorial = findFactorial(req.query.login);
+    var number = req.query.login;
+    function findFactorial(n) {
+        return n ? n * findFactorial(n - 1) : 1;
+    }
 
-  res.render('form-res', req.body);
+    var result = findFactorial(req.param('login'));
+    req.params.factorial = result;
+    // req.query.factorial = findFactorial(req.query.login);
 
-  res.send('<p> Факториал числа - ' + factorial + '.</p>');
+    // res.render('form-res', {'factorial' : result});
+    res.send('<p>hello ' + result + '</p>');
+
 
 });
 //
