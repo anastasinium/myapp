db6c1a7d82507e1f24fccae1f1f8b0d15fe00552
Anastasia <nastia21.11@gmail.com>
2017-06-22 00:17:36 +0300

    ..

--- routes/form.js
+++ routes/form.js
@@ -1,36 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-
-// function findFactorial(n) {
-//     return n ? n * findFactorial(n - 1) : 1;
-// }
-
 router.post('/', function (req, res) {
+    var number = req.param('num');
 
-    // req.query.factorial = findFactorial(req.query.login);
-    var number = req.query.login;
     function findFactorial(n) {
         return n ? n * findFactorial(n - 1) : 1;
     }
 
-    var result = findFactorial(req.param('login'));
-    req.params.factorial = result;
-    // req.query.factorial = findFactorial(req.query.login);
+    var result = findFactorial(number);
 
-    // res.render('form-res', {'factorial' : result});
-    res.send('<p>hello ' + result + '</p>');
-
-
+    res.render('form-res', {
+        num: number,
+        factorial : result
+    });
 });
-//
-
-// function (n) {
-//   return (n != 1) ? n * factorial(n - 1) : 1;
-// }
-
-// router.post('/', function (req, res) {
-//   res.render('form-res', req.body);
-// })
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', {
-  	title: 'Express',
-  	course: 'frontend pro',
-  	list: [{
-  		firstName: 'yura',
-  		lastName: 'fedorenko',
-  		age: 27
-  	}, {
-  		firstName: 'nastia',
-  		lastName: 'pidoprigora',
-  		age: 24
-  	}]
+  	title: 'Express'
    });
 });
 
